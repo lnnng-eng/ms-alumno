3637476fb0d9d6cd9a4054a6669431ed4694e456
Luanna Guajardo <gluannna@gmail.com>
2025-12-14 05:16:28 -0300

    Elimine metricas innecesarias y baje la carga

--- spike_tests.js
+++ spike_tests.js
@@ -1,19 +1,20 @@
 import http from 'k6/http';
-import { Trend } from 'k6/metrics';
 import { check, sleep } from 'k6';
-
-const statusTrend = new Trend('status_codes');
 
 export const options = {
     // Tu configuración de Traefik redirige a HTTPS y usa un certificado auto-firmado.
     insecureSkipTLSVerify: true,
-    // AJUSTE CRÍTICO: Reducimos la carga a un nivel manejable (20 usuarios).
-    // 10,000 usuarios saturarán el servidor de desarrollo y causarán timeouts.
+    // Spike test: subida rapida, sostenimiento y caida
     stages: [
-        { duration: "10s", target: 1000 },
-        { duration: "20s", target: 1000 },
-        { duration: "5s", target: 0 },
+        { duration: "10s", target: 200 }, // subida brusca
+        { duration: "2m", target: 200 }, // carga sostenida
+        { duration: "5s", target: 0 }, // bajada
     ],
+
+    thresholds: {
+        http_req_failed: ['rate<0.01'], //1% de errores
+        http_req_duration: ['p(95)<500'], //p95< 500ms
+    },
 };
 
 export default function () {
@@ -29,10 +30,6 @@ export default function () {
     //    No necesitamos 'payload' ni 'params' para una petición GET simple.
     const res = http.get(`${BASE_URL}/alumno`, params);
    
-
-    
-    statusTrend.add(res.status);
-
     check(res, {
         'status is 200': (r) => r.status === 200, // El endpoint /alumno debería devolver 200 OK
         'status is not 500': (r) => r.status !== 500, // Verificamos que no haya errores internos del servidor
@@ -42,3 +39,8 @@ export default function () {
     // Esto simula un comportamiento de usuario más realista y evita agotar los puertos de red del sistema operativo.
     sleep(1);
 }
+
+
+//elimine metricas innecesarias (Trend)
+//baje la carga de 1000 VUs a 200 VUs
+//agregue thresholds (lo cual es clave para justificar replicas)
