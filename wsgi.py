cfdd8dabdbf90a95ab2b25dff0d7d8af61af6f68
Luanna Guajardo <gluannna@gmail.com>
2025-12-14 22:21:27 -0300

    WSGI actualizado

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app
-app = create_app()
+import logging
 
-if __name__ == "__main__":
-    app.run()
-#bash           gunicorn wsgi:app
+app = create_app()
+app.app_context().push()
+
+
